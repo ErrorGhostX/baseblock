0af3d15586bc3096a2ef7ba6df6883fb44cf9f2f
ErrorGhostX <volnikov.yar@gmail.com>
2024-12-12 03:31:25 +0300

    Подключение MySQL
    Signed-off-by: ErrorGhostX <volnikov.yar@gmail.com>

--- MinecraftSite/settings.py
+++ MinecraftSite/settings.py
@@ -86,10 +86,14 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'BaseBlock',
+    'USER': 'root',
+    'PASSWORD': '30-30-30',
+    'HOST': 'localhost',
+    'PORT': 3306,
+}
 }
 
 CACHES = {
